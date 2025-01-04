6b086c5605353add15e15c52467a409658148de0
Ken-2511 <iwmain@outlook.com>
2025-01-04 10:22:40 -0500

    format the code using chatgpt

--- windows_service/cameraCap.h
+++ windows_service/cameraCap.h
@@ -5,16 +5,16 @@
 void setServerAddress(const std::string& address, int port);
 
 // 构建请求 URL
-std::string buildRequestURL(const std::string& endpoint, const std::string& params = "");
+std::string buildRequestURL(const std::string& endpoint, const std::map<std::string, std::string>& params);
 
 // 发送 GET 请求
 std::string sendGetRequest(const std::string& url);
 
 // 将获取的图片数据存储到文件
-void saveImage(const std::string& imageData, const std::string& filename);
+bool saveImage(const std::string& imageData, const std::string& filename);
 
 // 获取图片并保存
-void fetchAndSaveImage(const std::string& endpoint, const std::string& filename);
+bool fetchAndSaveImage(const std::string& endpoint, const std::string& filename);
 
 // 检查请求的响应是否有效
 bool isValidResponse(const std::string& response);

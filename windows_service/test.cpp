909eea72552d58b7fdac90d7c443e798ec011f48
Ken-2511 <iwmain@outlook.com>
2025-05-20 00:03:43 -0400

    test...

--- windows_service/test.cpp
+++ windows_service/test.cpp
@@ -1,42 +1,51 @@
 #include <windows.h>
+
 #include <iostream>
-#include "eventMonitor.h"
-#include "multiScreenCap.h"
+
 #include "cameraCap.h"
 #include "config.h"
+#include "eventMonitor.h"
+#include "multiScreenCap.h"
+#include "utils.h"
 
 int main() {
-    // 初始化所有模块
-    Config::load("..\\.env");
-    initializeDPI();
-    setServerAddress("192.168.68.127", 80);
+  // 初始化所有模块
+  Config::load("..\\.env");
+  initializeDPI();
+  setServerAddress("192.168.68.127", 80);
 
-    // 测试环境变量是否正常
-    cout << "API Key: " << Config::get("API_KEY") << endl;
+  // 测试环境变量是否正常
+  cout << "API Key: " << Config::get("API_KEY") << endl;
 
-    // 获取显示器数量
-    int monitorCount = getMonitorCount();
-    cout << "Monitor count: " << monitorCount << endl;
+  // 获取显示器数量
+  int monitorCount = getMonitorCount();
+  cout << "Monitor count: " << monitorCount << endl;
 
-    // 获取当前日期和时间
-    wstring date = getDateString();
-    wstring time = getTimeString();
-    wcout << L"Date: " << date << L", Time: " << time << endl;
+  // 获取当前日期和时间
+  wstring date = getDateString();
+  wstring time = getTimeString();
+  wcout << L"Date: " << date << L", Time: " << time << endl;
 
-    // 向树莓派发送请求
-    fetchAndSaveImage("photo", L"image.jpg");
+  // 向树莓派发送请求
+  fetchAndSaveImage("photo", L"image.jpg");
 
-    // 测试鼠标事件监控
-    EventMonitor monitor;
-    for (int i = 0; i < 10; i++) {
-        monitor.update();
-        if (monitor.hasMoved(1000)) {
-            cout << "Mouse moved!" << endl;
-        } else {
-            cout << "Mouse not moved!" << endl;
-        }
-        Sleep(1000);
+  // 截取所有显示器
+  captureAllMonitors(L".", time);
+
+  // 将截到的图片保存为PNG格式
+  bmp2png("monitor_____DISPLAY1.bmp", "monitor.png");
+
+  // 测试鼠标事件监控
+  EventMonitor monitor;
+  for (int i = 0; i < 10; i++) {
+    monitor.update();
+    if (monitor.hasMoved(1000)) {
+      cout << "Mouse moved!" << endl;
+    } else {
+      cout << "Mouse not moved!" << endl;
     }
+    Sleep(1000);
+  }
 
-    return 0;
+  return 0;
 }
